5704ed1f07eee0a64b7f795afecb9b1b79bcd049
Sam Brook <brookyy@gmail.com>
2014-03-26 21:04:03 +1100

    Completed dom events.

--- hands-on/js/main.js
+++ hands-on/js/main.js
@@ -13,22 +13,62 @@
 
 	App.Models.Task = Backbone.Model.extend({});
 
+	App.Collections.Tasks = Backbone.Collection.extend({
+		model: App.Models.Task
+	});
+
+	App.Views.Tasks = Backbone.View.extend({
+		tagName: 'ul',
+
+		render: function(){
+			this.collection.each(this.addOne, this);
+		},
+
+		addOne: function(task){
+			// Creating a new child view & Append to root element
+			var taskView = new App.Views.Task({ model: task});
+			this.$el.append(taskView.render().el);
+		}
+	});
+
 	App.Views.Task = Backbone.View.extend({
 		tagName: 'li',
 
+		events: {
+			'click .edit': 'editTask'
+		},
+
+		editTask: function(){
+			var newTask = prompt('Change text to', this.model.get('title'));
+			this.model.set('title', newTaskTitle);
+		},
+
+		template: template('taskTemplate'),
+
 		render: function(){
-			this.$el.html( this.model.get('title') );
+			var template = this.template(this.model.toJSON());
+			this.$el.html( template );
 			return this;
 		}
 	});
 
-	var task = new App.Models.Task({
-		title: 'Go to the store',
-		priority: 4
-	});
+	var tasksCollection = new App.Collections.Tasks([
+		{
+			title: 'Go to the store',
+			priority: 4
+		},
+		{
+			title: 'Go to the Mall',
+			priority: 3
+		},
+		{
+			title: 'Go to the work',
+			priority: 54
+		}
+	]);
 
-	var taskView = new App.Views.Task({ model: task });
-
-	console.log( taskView.render().el );
-
+	var tasksView = new App.Views.Tasks({ collection: tasksCollection });
+	tasksView.render();
+	console.log(tasksView.el);
+	$(".tasks").html(tasksView.el);
 })();
