4b7f208772e4f8d95b096d992e3bd2ce9694dfa8
Sam Brook <brookyy@gmail.com>
2014-03-27 22:06:40 +1100

    Finished hands on.

--- hands-on/js/main.js
+++ hands-on/js/main.js
@@ -11,7 +11,13 @@
 		return _.template($('#' + id ).html());
 	};
 
-	App.Models.Task = Backbone.Model.extend({});
+	App.Models.Task = Backbone.Model.extend({
+		validate: function(attrs){
+			if( ! $.trim(attrs.title)){
+				return 'A task requires a valid title';
+			}
+		}
+	});
 
 	App.Collections.Tasks = Backbone.Collection.extend({
 		model: App.Models.Task
@@ -19,6 +25,10 @@
 
 	App.Views.Tasks = Backbone.View.extend({
 		tagName: 'ul',
+
+		initialize: function() {
+			this.collection.on('add', this.addOne, this);
+		},
 
 		render: function(){
 			this.collection.each(this.addOne, this);
@@ -38,23 +48,59 @@
 
 		initialize: function() {
 			this.model.on('change', this.render, this);
+			this.model.on('destroy', this.remove, this);
 		},
 
 		events: {
-			'click .edit': 'editTask'
+			'click .edit'	: 'editTask',
+			'click .delete'	: 'destroy'
 		},
 
 		editTask: function(){
 			var newTaskTitle = prompt('Change text to', this.model.get('title'));
 			console.log('testing',this.model);
 			//this.model.set({title: newTaskTitle}, {validate: true});
-			this.model.set("title", newTaskTitle);
+			if( !newTaskTitle ) return;
+
+			this.model.set({title: newTaskTitle}, {validate: true});
+		},
+
+		destroy: function(){
+			this.model.destroy();
+			console.log(tasksCollection);
+		},
+
+		remove: function(){
+			this.$el.remove();
 		},
 		
 		render: function(){
 			var template = this.template(this.model.toJSON());
 			this.$el.html( template );
 			return this;
+		}
+	});
+
+	App.Views.AddTask = Backbone.View.extend({
+		el: '#addTask',
+
+		events: {
+			'submit': 'submit'
+		},
+
+		initialize: function(){
+			//console.log(this.el.innerHTML);
+		},
+
+		submit: function(e){
+			e.preventDefault();
+			console.log('submited');
+
+			var newTaskTitle = $(e.currentTarget).find('input[type=text]').val();
+
+			var task = new App.Models.Task({ title: newTaskTitle });
+			this.collection.add(task);
+			console.log(task);
 		}
 	});
 
@@ -73,6 +119,7 @@
 		}
 	]);
 
+	var addTaskView = new App.Views.AddTask({ collection: tasksCollection })
 	var tasksView = new App.Views.Tasks({ collection: tasksCollection });
 	tasksView.render();
 	console.log(tasksView.el);
